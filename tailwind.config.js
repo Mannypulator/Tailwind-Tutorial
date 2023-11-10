d77315af8722163c43562c1e6e3331a92332e723
Mannypulator <emeritus93@gmail.com>
2023-11-10 19:36:31 +0100

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "cover-image": "url('/build/img/contact-bg.jpeg')",
+        "main-image": "url('/build/img/Bgpicture3.jpeg')",
+      }),
+    },
   },
   plugins: [],
 };
